843fe4e218aca1c31f25a5c16da4f570ab95cbb4
Martin Musale <martinmshale@gmail.com>
2018-03-22 16:21:48 +0300

    Return error when the yaml file does not exist

--- example/index.js
+++ example/index.js
@@ -2,16 +2,21 @@
 
 const Koa = require('koa');
 const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
 const USSD = require('../dist').default;
 
-const filePath = "./test.yml"
+const filePath = './test.yml';
 const app = new Koa();
 const router = new Router();
 const menuScreens = new USSD(filePath);
 
+app.use(BodyParser());
+
 router.post('/ussd', async (ctx, next) => {
-  menuScreens.run(ctx.body, response => {
-    ctx.body = response;
+  menuScreens.run(ctx.request.body, (err, response) => {
+    if (err) console.error(err);
+    else ctx.body = response;
+    next();
   });
 });
 
