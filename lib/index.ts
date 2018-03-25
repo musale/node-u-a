88319a2ed37cc0f4ec81c8969be06f10d85bbf30
Martin Musale <martinmshale@gmail.com>
2018-03-25 16:27:49 +0300

    Fix validation of initial screen

--- lib/index.ts
+++ lib/index.ts
@@ -1,55 +1,24 @@
-import { readYamlFile } from './tools/tools';
-import { CustomErrors } from './tools/errors';
-import { YamlValues } from './tools/types';
+import { readYamlFile, validateInitialScreen } from './tools/tools';
 
 class USSD {
   filePath: string;
-  screenValues: any;
+  yamlContent: any;
   ussdPath: string;
   constructor(filePath: string) {
     this.filePath = filePath;
   }
 
-  async setUp() {
-    // load yaml file
-    try {
-      const values = await readYamlFile(this.filePath);
-      this.screenValues = values;
-      return await this.validateYamlValues(values);
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
   async run(body, callback) {
     try {
-      const validMessage: string = await this.setUp();
-      if (validMessage === 'valid') {
-        this.ussdPath = body;
-        return callback(null, `Welcome`);
-      } else {
-        return callback(validMessage, null);
-      }
+      const yamlContent = await readYamlFile(this.filePath);
+      this.yamlContent = yamlContent;
+      await validateInitialScreen(this.yamlContent);
+      return callback(null, 'Welcome');
     } catch (error) {
-      if (error instanceof CustomErrors) {
-        const { message, stage } = error;
-        return callback({ message, stage }, null);
-      }
-      return callback(error.message, null);
+      console.error();
+      return callback(new Error(error), null);
     }
   }
-
-  validateYamlValues(values): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const keys = Object.keys(values);
-      const initialScreenExists = keys.find(
-        names => names === 'initial_screen'
-      );
-      if (initialScreenExists === undefined)
-        reject(
-          new CustomErrors('An initial_screen is required', 'yaml_validation')
-        );
-      return resolve(`valid`);
-    });
-  }
 }
+
 export default USSD;
